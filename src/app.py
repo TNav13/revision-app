fb324e6cea62bbaddb8526f6c5d81b16822fcdae
TNav13 <theo.navailles@icloud.com>
2026-07-08 10:26:29 +0200

    docs: ajout modif black

--- src/app.py
+++ src/app.py
@@ -1,15 +1,20 @@
 # src/app.py
 from flask import Flask, jsonify
+
 __version__ = "1.0.0"
 
 app = Flask(__name__)
+
+
 @app.get("/")
 def index():
     return jsonify(message="Usine logicielle", version=__version__)
 
+
 @app.get("/health")
 def health():
     return jsonify(status="ok"), 200
+
 
 @app.get("/version")
 def version():
